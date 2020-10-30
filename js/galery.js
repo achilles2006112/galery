38ee1c65cc4d6ab8ff1d7c8d28f621a99cf0d212
galery <acihllesal@psagot.ort.org.il>
2020-10-30 16:42:39 +0200

    it's my galery!

---
+++ js/galery.js
@@ -0,0 +1,30 @@
+window.addEventListener("load",function(){
+	var img = document.querySelector("#photo");
+	var btnf = document.querySelector("#forward"); 
+	var btnb = document.querySelector("#back");
+	var list2 = document.querySelector("#category");
+	var category = "boys/";
+	var counter = 1;
+	list2.addEventListener("change", function(){
+			category = this.value + "/";
+			counter = 1; 
+			img.src = "images/" + category + counter + ".jpg";
+	});
+	btnf.addEventListener("click",function(){
+		counter++;
+		if(counter>4){
+			counter=1;			
+		}
+		img.src = "images/"+category+"/"+counter+".jpg";
+		
+	});
+	
+	btnb.addEventListener("click",function(){
+		counter--;
+		if(counter<1){
+			counter = 4;
+		}
+		img.src = "images/"+category+"/"+counter+".jpg";
+	});
+	
+});
